fe14c851954490d747de553d6682e820e241d541
dealmirante <dealmirante@gmail.com>
2020-07-17 18:18:29 -0300

    Se lista la Api con Spinner mas bootstrap

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import ListadoPersonajes from './components/ListadoPersonajes';
 function App() {
   return (
 
-    <React.Fragment>
-      <h1> Hola Mundo </h1>
+    <div className='container'>
+
+      <h1> Proyecto en React </h1>
+
+      <h2> Start Wars Personajes Api </h2>
 
       <ListadoPersonajes dePrueba = "Hoy es el dia 119 de la cuarentena" >
         <h3> Hola mundo, esto es un children </h3>
         <h3> Hola mundo, esto es otro children </h3>
       </ListadoPersonajes>
 
-    </React.Fragment>
+    </div>
   );
 }
 export default App;
--- src/components/ListadoPersonajes.js
+++ src/components/ListadoPersonajes.js
@@ -6,18 +6,80 @@ class ListadoPersonajes extends Component {
         super(props);
         this.state = {
             titulo: 'Soy el componente Listado de Personajes',
-            MiEstado: 'En cuarentena eterna'
+            MiEstado: 'En cuarentena eterna',
+            Personajes: [],
+            Loading: true
         }
     }   
+    // Ciclos de vida de un componente
+    // 1.- Constructor
+    // 2.- static getDerivedStateFromProps
+    // 3.- render
+    // 4.- componentDidMount     
+
+    componentDidMount () {
+        console.log('entro a componentDidMount' );
+        fetch('https://swapi.dev/api/people/?format=json')
+            .then(Response => Response.json())
+            .then(data => {
+                // seteo el estado despues que el componente se monto 1:20:28 
+                this.setState({
+                    Personajes: data.results,
+                    Loading: false
+                })
+                // fin del seteo
+            })
+            .catch(error => console.error(error))        
+    }
+
+    componentDidUpdate () {        
+    }
+
 
         render () {
-            let {titulo, MiEstado} = this.state;
+            let {titulo, MiEstado, Personajes, Loading} = this.state;
             return (
             <React.Fragment>
-                 <h4> { titulo } </h4>    
+                 <h4> { titulo } </h4>   
                  <p> {this.props.dePrueba}</p>
                  <p> { MiEstado } </p> 
                  { this.props.children }
+
+                 {Loading 
+                  ?
+                 <div className="spinner-border text-primary" role="status">
+                 <span className="sr-only">Loading...</span>
+                 </div>
+                 :
+                 <table className="table">
+                 <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Full name</th>                    
+                    <th scope="col">Gender</th>
+                    <th scope="col">URL</th>
+                    </tr>
+                 </thead>
+                 <tbody>
+                       {
+                         Personajes.map((unPersonaje, indice) => {
+                         return (
+                         <tr key={indice}> 
+                         <th scope="row">{indice + 1}</th>
+                         <td>{unPersonaje.name} </td>
+                         <td>{unPersonaje.gender} </td>
+                         <td>
+                             <a href={unPersonaje.url} target="_blank" rel="noopener noreferrer">
+                                 Ver Detalle
+                             </a>
+                         </td>                         
+                         </tr>
+                         )
+                         } )
+                       }                                    
+                 </tbody>
+                 </table>
+                 }
             </React.Fragment>
                
             );
